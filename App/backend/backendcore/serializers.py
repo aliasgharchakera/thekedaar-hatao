5e436e5e92f8daa95600f1f2442a38777538feb1
Moody <mm06600@st.habib.edu.pk>
2023-04-10 12:33:23 +0500

    models

--- App/backend/backendcore/serializers.py
+++ App/backend/backendcore/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework.serializers import ModelSerializer
-<<<<<<< HEAD
 from .models import Uuser
 
 
@@ -8,12 +7,3 @@ class Uuserserializer(ModelSerializer):
     class Meta:
         model = Uuser
         fields = '__all__'
-=======
-from .models import User
-
-
-class Userserializer(ModelSerializer):
-    class Meta:
-        model = User
-        field = '__all__'
->>>>>>> 8e052e21a33856780a0c495f2c938c497e8b4b27
