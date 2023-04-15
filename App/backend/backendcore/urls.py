015d0d2f321386f748f81fe0f6509ef45ce095fa
Ali Asghar <aliasghar.chakera@gmail.com>
2023-04-15 15:11:00 +0500

    Django default user login and signup used

--- App/backend/backendcore/urls.py
+++ App/backend/backendcore/urls.py
@@ -1,12 +1,19 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.hello_django, name='hellodjango'),
+#     path('hello/', views.hello_django, name='hellodjango'),
+#     path('uusers/',views.getUuser),
+#     path('user/create/', views.createUuser),
+#     # path ('')
+# ]
+
 from django.urls import path
-from . import views
-
+from .views import UserDetailAPI,RegisterUserAPIView, my_view, signup_view
 urlpatterns = [
-    path('', views.hello_django, name='hellodjango'),
-    path('hello/', views.hello_django, name='hellodjango'),
-    path('uusers/',views.getUuser),
-    path('user/create/', views.createUuser),
-    # path ('')
-
-
+  path("get-details",UserDetailAPI.as_view()),
+  path('register',RegisterUserAPIView.as_view()),
+  path('login', my_view),
+  path('signup', signup_view)
 ]
--- App/backend/backendcore/views.py
+++ App/backend/backendcore/views.py
@@ -1,45 +1,97 @@
-from django.http import HttpResponse
-from django.shortcuts import render, HttpResponse
-from rest_framework.response import Response
+# from django.http import HttpResponse
+# from django.shortcuts import render, HttpResponse
+# from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
-from .models import Uuser
-# from django.contrib.auth.models import Uuser
-from .serializers import Uuserserializer
+# from .models import Uuser
+# # from django.contrib.auth.models import Uuser
+# from .serializers import Uuserserializer
 
-# from .models import User
+# # from .models import User
 
 
-# Create your views here.
+# # Create your views here.
 
-@api_view(['GET'],)
-@permission_classes([AllowAny],)
-def hello_django(request):
-    print("Request received")
-    return Response({'message: Request Successfully returned! Hello Django!'}, status = 200)
+# @api_view(['GET'],)
+# @permission_classes([AllowAny],)
+# def hello_django(request):
+#     print("Request received")
+#     return Response({'message: Request Successfully returned! Hello Django!'}, status = 200)
 
-@api_view(['GET'],)
-@permission_classes([AllowAny],)
-def getUuser(request):
-    Uusers = Uuser.objects.all()
-    serializer = Uuserserializer(Uusers,many = True)
-    return Response(serializer.data)
+# @api_view(['GET'],)
+# @permission_classes([AllowAny],)
+# def getUuser(request):
+#     Uusers = Uuser.objects.all()
+#     serializer = Uuserserializer(Uusers,many = True)
+#     return Response(serializer.data)
 
+
+# @api_view(['POST'])
+# @permission_classes([AllowAny],)
+# def createUuser(request):
+#     data = request.data
+#     user = Uuser.objects.create(
+#         email = data['email'],
+#         password = data['password'],
+#         # contact = data['contact'],
+#     )
+#     print(user.email, '\n', user.password)
+#     serializer = Uuserserializer(user,many=False)
+#     return Response(serializer.data, status=201)
+
+
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer,RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import generics
+from django.contrib.auth import authenticate, login
 
 @api_view(['POST'])
 @permission_classes([AllowAny],)
-def createUuser(request):
+def my_view(request):
     data = request.data
-    user = Uuser.objects.create(
-        email = data['email'],
-        password = data['password'],
-        # contact = data['contact'],
-    )
-    print(user.email, '\n', user.password)
-    serializer = Uuserserializer(user,many=False)
-    return Response(serializer.data, status=201)
+    username = data['username']
+    password = data['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        return Response({'login successfully'}, status = 200)
+    else:
+        # Return an 'invalid login' error message.
+        return Response({'login failed'}, status = 201)
+      
+@api_view(['POST'])
+@permission_classes([AllowAny],)  
+def signup_view(request):
+    data = request.data
+    username = data['username']
+    email = data['email']
+    password = data['password']
+    first_name = data['first_name']
+    last_name = data['last_name']
+    user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+    if user is not None:
+        user.save()
+        return Response({'user created successfully'}, status = 200)
+    return Response({'user created failed'}, status = 201)
 
+# Class based view to Get User Details using Token Authentication
+class UserDetailAPI(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (AllowAny,)
+    def get(self,request,*args,**kwargs):
+        user = User.objects.get(id=request.user.id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
 
+#Class based view to register user
+class RegisterUserAPIView(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
 
 
 
