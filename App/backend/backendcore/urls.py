499b9e46410ff28f932a1be55b1fe60acda7f4ee
Ali Asghar <aliasghar.chakera@gmail.com>
2023-04-15 15:29:59 +0500

    Cleaning up uneccessary code

--- App/backend/backendcore/urls.py
+++ App/backend/backendcore/urls.py
@@ -1,17 +1,6 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.hello_django, name='hellodjango'),
-#     path('hello/', views.hello_django, name='hellodjango'),
-#     path('uusers/',views.getUuser),
-#     path('user/create/', views.createUuser),
-#     # path ('')
-# ]
-
 from django.urls import path
 from .views import login_view, signup_view
 urlpatterns = [
-  path('login', login_view),
-  path('signup', signup_view)
+    path('login', login_view),
+    path('signup', signup_view)
 ]
